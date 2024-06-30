fb548284f2d19f0f7f9131d55e1cb1a207e6b2d8
akanshamore <akanshamore21@gmail.com>
2024-06-30 23:25:41 +0530

    imported icons from material UI

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import twitterImage from "../../assets/images/twitter.jpeg";
 
 const Login = () => {
 
 
     return (
 
-        <div>
-
-            <h1>This is login page</h1>
+        <div className='login-container'>
+            <div className='image-container'>
+                <img src={twitterImage} alt="" />
+            </div>
+            <div className='form-container'>
+                <TwitterIcon />
+                <h2>Happening now</h2>
+                <form>
+                    <input type="email" />
+                    <input type="password" />
+                    <div className='btn-login'>
+                        <button type="submit" className='="btn'>
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Login;
